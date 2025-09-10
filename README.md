46d2348f7270fd09d51bf22f93c25053d5e19312
wesleivisentin <78518664+wesleivisentin@users.noreply.github.com>
2025-09-10 21:38:58 +0000

    adiciona scripts  and

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Project clone tab news
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Daniela io ti amo</h1>
+  return <h1>Daniela io ti amo</h1>;
 }
 
-export default Home
+export default Home;
